80a7c4530d4abdc45fe45019bc35bada21397fd6
Travis Craft <tlcraft@users.noreply.github.com>
2026-04-07 13:31:25 -0600

    Add product test.

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -1,5 +1,22 @@
-describe("it runs", () => {
-  it("says hello", () => {
-    expect(true).toBeTruthy();
+class Product {
+  id: number;
+  stock: number;
+  hold: number;
+
+  constructor(id: number, stock: number) {
+    this.id = id;
+    this.stock = stock;
+    this.hold = 0;
+  }
+}
+
+describe("add item to order", () => {
+  describe("sufficient product stock", () => {
+    it("should create a new product", () => {
+      const product = new Product(327, 7);
+      expect(product.hold).toBe(0);
+    });
   });
 });
+
+
