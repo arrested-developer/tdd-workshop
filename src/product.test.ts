26847f0f6fe568475beb1d94249038b37fc5d246
Michael Watts <mwatts@greenprojecttech.com>
2026-04-07 20:41:34 +0100

    add order class

--- src/product.test.ts
+++ src/product.test.ts
@@ -1,12 +1,23 @@
 import { Product } from "./product";
 
+class Order {
+  add(product: Product) {
+    product.hold = 1;
+  }
+}
+
 describe("add item to order", () => {
   describe("sufficient product stock", () => {
     it("should create a new product", () => {
       const product = new Product(327, 7);
       expect(product.hold).toBe(0);
     });
+
+    it("adds product to order", () => {
+      const product = new Product(327, 7);
+      const order = new Order();
+      order.add(product);
+      expect(product.hold).toBe(1);
+    });
   });
 });
-
-
